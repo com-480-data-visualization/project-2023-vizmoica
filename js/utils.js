7d4b0bd256935e905d459eada38c020b8611755a
julien <julien.mettler@epfl.ch>
2023-06-02 17:36:53 +0200

    Refactor counter code

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,31 @@
+/**
+ * Animates a counter from a start value to an end value.
+ * 
+ * @param {*} selector  The selector of the element to animate
+ * @param {*} startValue  The start value
+ * @param {*} endValue  The end value
+ * @param {*} duration  The duration of the animation in milliseconds
+ */
+function animateCounter(selector, startValue, endValue, duration = 2000) {
+    d3.selectAll(selector)
+        .transition()
+        .tween("text", function () {
+            let node = this;
+            const interpolator = d3.interpolateNumber(startValue, endValue);
+            return function (t) {
+                d3.select(node)
+                    .text(Math.round(interpolator(t)).toLocaleString("en-US"))
+            };
+        })
+        .duration(duration);
+}
+
 // URL of the default image to use when an image is not found
 const DEFAULT_IMG_URL = "data/graph3_map/no_picture_mal.png";
 
-// Adds a comma for every thousands, e.g., converts 123456 to "123,456"
+/**
+ * Adds a comma for every thousands, e.g., converts "123456" to "123,456"
+ */
 let formatAsThousands = d3.format(",")
 
 function formatAsDays(days) {
@@ -13,12 +37,12 @@ function formatAsDays(days) {
     return `${xDays} days, ${xHours} hours, and ${xMinutes} minutes`;
 }
 
-
 /**
- * https://dev.to/jorik/country-code-to-flag-emoji-a21
+ * Returns the flag emoji string for a given country.
+ * Adapted from https://dev.to/jorik/country-code-to-flag-emoji-a21.
  * 
- * @param {*} countryCode 
- * @returns 
+ * @param {*} countryCode  The country code
+ * @returns  The flag emoji
  */
 function getFlagEmoji(countryCode) {
     const codePoints = countryCode
