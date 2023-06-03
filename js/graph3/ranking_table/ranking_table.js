ab9a2e00ad70125e6f4efbd5c7bdda2603f0a27a
julien <julien.mettler@epfl.ch>
2023-06-03 09:50:09 +0200

    Redo ranking table

--- js/graph3/ranking_table/ranking_table.js
+++ js/graph3/ranking_table/ranking_table.js
@@ -1,61 +1,36 @@
-function initRankings() {
-    d3.select("#" + rankingId).selectAll("*").remove();
-}
+// function initRankings() {
+//     d3.select("#" + rankingId).selectAll("*").remove();
+// }
 
 function updateRankings(rankingData, rankingId, headerA, headerB, colA, colB, num_rows = 10) {
     let rankings = d3.select("#" + rankingId);
     rankings.selectAll("*").remove();
 
+    let table = rankings.append("table")
 
-    let table = rankings
-        .selectAll("table")
-        .data([rankingData.slice(0, num_rows)]);
-
-
-    let tableEnter = table.enter()
-        .append("table");
-
-    tableEnter
-        .append("caption")
-        .text("The favorite studios");
-
-    let thead = tableEnter
-        .append("thead");
-
-    let thead_tr = thead
-        .append("tr");
-
+    /* Table header */
+    let thead = table.append("thead");
+    let thead_tr = thead.append("tr");
     thead_tr.append("th").text("Ranking");
     thead_tr.append("th").text(headerA);
     thead_tr.append("th").text(headerB);
 
-    let tbody = tableEnter
-        .append("tbody");
-
-    let rows = tbody
-        .selectAll("tr")
-        .data(d => d);
-
-    rows.exit().remove();
-
-    let rowsEnter = rows.enter()
+    /* Table body */
+    let tbody = table.append("tbody");
+    let rows = tbody.selectAll("tr")
+        .data(rankingData.slice(0, num_rows))
+        .enter()
         .append("tr")
-        .style("opacity", 0);
 
-    rowsEnter.merge(rows)
-        .transition()
-        .duration(500)
-        .style("opacity", 1);
-
-    rowsEnter.append("td")
+    // Column 1: ranking
+    rows.append("td")
         .text((_, i) => i + 1);
-
-    rowsEnter.append("td")
+    // Column 2: studio name
+    rows.append("td")
         .text(d => d[colA]);
-
-    rowsEnter.append("td")
+    // Column 3: number of ratings
+    rows.append("td")
         .text(d => formatAsThousands(d[colB]));
 
-    table.exit().remove();
-
+    // rows.exit().remove();
 }
